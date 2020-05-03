9364d718872700385cde98829ca6cdd4deb2ab66
Brianna Williams <bjw2135@columbia.edu>
2020-05-03 02:05:40 -0400

    Started interactive

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Classifier of American Sign Language hand movements for translation
+"""
+
+
+import cv2
+import numpy
+
+
+def aslToEnglish(image):
+    letter = 0
+    return letter
+
+
+if __name__ == '__main__':
+    print("Input your letter:")
+    image = input()
+    eng = aslToEnglish(image)
+    print("The letter you're looking for is: " + eng)
