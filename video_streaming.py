558744dd5e088cb3c13ac6ac88d5dea4ffb030c7
nykekana <45674574+nykekana@users.noreply.github.com>
2020-05-09 17:27:54 -0400

    hand skin mask
    video streaming with box for hand with hand mask that detects skin

--- video_streaming.py
+++ video_streaming.py
@@ -6,6 +6,7 @@ cap = cv2.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # print(frame)
     # print(type(frame))
@@ -25,10 +26,27 @@ while(True):
     cropped_w, cropped_h = int(w/2), int(h/2)
 
     crop_img = frame[cropped_w:, : cropped_h]
+
+    lower = np.array([0, 48, 80], dtype = "uint8")
+    upper = np.array([20, 255, 255], dtype = "uint8")
+    # mask = cv2.inRange(crop_img, lower, upper)
+
+    converted = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+    skinMask = cv2.inRange(converted, lower, upper)
+    # apply a series of erosions and dilations to the mask
+    # using an elliptical kernel
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+    skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+    # blur the mask to help remove noise, then apply the
+    # mask to the frame
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+    skin = cv2.bitwise_and(crop_img, crop_img, mask = skinMask)
     
     # Display the resulting frame
     cv2.imshow('frame',frame)
     cv2.imshow("cropped", crop_img)
+    cv2.imshow("skin", skin)
     
     # press q to stop video 
     if cv2.waitKey(1) & 0xFF == ord('q'):
