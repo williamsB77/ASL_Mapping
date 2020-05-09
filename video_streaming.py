b0442a7d4528080b3dac331b9747d70531e3ce53
nykekana <45674574+nykekana@users.noreply.github.com>
2020-05-09 17:13:49 -0400

    has box for the hand and the cropped image

--- video_streaming.py
+++ video_streaming.py
@@ -7,18 +7,28 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    print(frame)
-    print(type(frame))
-
+    # print(frame)
+    # print(type(frame))
+    
     w,h,d = frame.shape
     w,h,d = int(w), int(h), int(d)
     # print(type(w), type(h), type(d))
 
-    # draw rectangle
-    cv2.rectangle(frame,(int(w/2),int(w/2)),(int(h/2),int(h/2)),(0,255,0),7)
+    radius = 15
+    thickness = 7
 
+    # draw rectangle -- #TODO make rectangle bigger
+    start_point = (int(h/2),int(w/2))
+    end_point = (0,w)
+    cv2.rectangle(frame,start_point,end_point,(0,255,0),7)
+
+    cropped_w, cropped_h = int(w/2), int(h/2)
+
+    crop_img = frame[cropped_w:, : cropped_h]
+    
     # Display the resulting frame
     cv2.imshow('frame',frame)
+    cv2.imshow("cropped", crop_img)
     
     # press q to stop video 
     if cv2.waitKey(1) & 0xFF == ord('q'):
