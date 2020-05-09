b2407450f20afadd36823074e0d1c2beda84b9c2
nykekana <45674574+nykekana@users.noreply.github.com>
2020-05-09 18:36:51 -0400

    prints out the convex hull of a single image

---
+++ hand_convex_hull.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+im_name = 'a.PNG'
+def get_convex_hull(im_filename):
+    im = cv2.imread(im_name)
+
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(im, contours, -1, (0,255,0), 3)
+
+    cnt = contours[1]
+    cv2.drawContours(im, [cnt], 0, (0,255,0), 3)
+
+    hull = cv2.convexHull(cnt)
+    hull_area = cv2.contourArea(hull)
+
+    return(hull_area)
+
+    # to see the image with the counters 
+    # cv2.imshow('frame',im)
+
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+print(get_convex_hull(im_name))
